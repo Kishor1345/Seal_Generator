d070d0a6e6a3450ca128a97d5c6c1904f81b6825
kishor1345 <kishoredinesh0803@gmail.com>
2026-02-26 15:12:07 +0530

    pages code added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Seal_Generator/'
+  base: '/ySeal_Generator/'
 })
